3270e7418330e3be186ecf6b9631615a058a72c4
NULGEN <git@stackblitz.com>
2024-11-03 01:56:25 +0300

    Initial commit

---
+++ src/hooks/useDarkMode.jsx
@@ -0,0 +1,6 @@
+import { useLocalStorage } from './useLocalStorage';
+
+export const useDarkMode = () => {
+  const [darkMode, setDarkMode] = useLocalStorage('geceModu', false);
+  return [darkMode, setDarkMode];
+};
